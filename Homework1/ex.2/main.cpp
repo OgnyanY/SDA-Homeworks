523ddb6e88bc7ede7afb488371d9b51e0083b1a1
OgnyanY <o.yonchev@abv.bg>
2021-10-10 23:46:48 +0300

    finale solution with comments

--- Homework1/ex.2/main.cpp
+++ Homework1/ex.2/main.cpp
@@ -2,26 +2,29 @@
 #include <vector>
 #include <algorithm>
 int FootballTeam (std::vector<int> arr, const int size){
-    std::sort(arr.begin(), arr.end());
-    bool positive = false;
+    std::sort(arr.begin(), arr.end());//sort the array
+    bool positive = false;//this is to check if the digit is positive
 
-    for(int i = 0;i < size;++i){
+    for(int i = 0; i < size; ++i){
+        //check if the digit is negative
         if(arr[i] <= 0){
             continue;
         }
-
         positive = true;
-
+        //check if there is smaller possible digit than the first positive-1
         if(arr[i-1] <= 0 && arr[i] > 1){
             return 1;
         }
+        //check for possible digit
         if(arr[i] + 1 < arr[i + 1]){
             return arr[i] + 1;
         }
+        //this is when there is no possible digit than the biggest+1
         if(arr[i] == arr[size-1]){
             return arr[i]+1;
         }
     }
+    //if there is only negative digits
     if(!positive){
         return 1;
     }
@@ -32,7 +35,7 @@ int main() {
     int number = 0;
     std:: cin >> number;
     std::vector<int> arr;
-    for(int i = 0; i < number;++i){
+    for(int i = 0; i < number; ++i){
         int num;
         std::cin >> num;
         arr.push_back(num);
