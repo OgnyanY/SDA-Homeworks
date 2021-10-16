d74eff2b89a088def2b19489b7751f4b76e06690
OgnyanY <o.yonchev@abv.bg>
2021-10-17 02:56:15 +0300

    finale version for ex.3 with comments

--- Homework2/ex.3/main.cpp
+++ Homework2/ex.3/main.cpp
@@ -3,26 +3,30 @@
 void merge(char arr[], char mergeArr[], int start, int mid, int end) {
     int left = start;
     int right = mid + 1;
-
+    //sort the elements
     for(int i = start; i <= end; ++i) {
+        //this is when the left element is smaller
         if(left <= mid && (right > end || arr[left] <= arr[right])){
             mergeArr[i] = arr[left];
             ++left;
-        }else {
+        }else {//this is when the right element is smaller
             mergeArr[i] = arr[right];
             ++right;
         }
     }
-
+    //assign the already sorted elements
     for(int i = start; i <= end; ++i) {
         arr[i] = mergeArr[i];
     }
 }
 
 void mergeSortRecursion(char arr[], char mergeArr[], int start, int end) {
+    //if there is only one element return
     if(start < end) {
         int mid = (start + end) / 2;
 
+        //divide the array by two and continue until there is only two elements in the array
+        //and then merge them
         mergeSortRecursion(arr, mergeArr, start, mid);
         mergeSortRecursion(arr, mergeArr, mid + 1, end);
 
@@ -31,7 +35,8 @@ void mergeSortRecursion(char arr[], char mergeArr[], int start, int end) {
 }
 
 void mergeSort (char arr[], int length) {
-    char* mergeArr = new char[length];
+    char* mergeArr = new char[length];//this is the array where we merge
+    //this is the recursive function for merging
     mergeSortRecursion(arr, mergeArr, 0, length - 1);
     delete[] mergeArr;
 }
@@ -40,47 +45,50 @@ int main() {
     int size = 0;
     std::cin >> size;
     char* arr = new char [size];
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; ++i) {
         std::cin >> arr[i];
     }
 
+    //create arrays for numbers, big letters and small letters
     char* num = new char [size];
     int numSize = 0;
-    char* bigL = new char [size];
-    int bigLSize = 0;
-    char* smallL = new char [size];
-    int smallLSize = 0;
+    char* bigLetters = new char [size];
+    int bigLettersSize = 0;
+    char* smallLetters = new char [size];
+    int smallLettersSize = 0;
 
-    for (int i = 0; i < size; i++) {
+    //assign the elements where they have to go
+    for (int i = 0; i < size; ++i) {
         if(arr[i] >= '0' && arr[i] <= '9'){
             num[numSize] = arr[i];
             ++numSize;
         }
         else if(arr[i] >='a' && arr[i] <= 'z'){
-            smallL[smallLSize] = arr[i];
-            ++smallLSize;
+            smallLetters[smallLettersSize] = arr[i];
+            ++smallLettersSize;
         }
         else if(arr[i] >='A' && arr[i] <= 'Z'){
-            bigL[bigLSize] = arr[i];
-            ++bigLSize;
+            bigLetters[bigLettersSize] = arr[i];
+            ++bigLettersSize;
         }
     }
-
+    //merge the separate arrays
     mergeSort(num, numSize);
-    mergeSort(bigL, bigLSize);
-    mergeSort(smallL, smallLSize);
+    mergeSort(bigLetters, bigLettersSize);
+    mergeSort(smallLetters, smallLettersSize);
 
-    for (int i = 0; i < numSize; i++) {
+    //cout the arrays in the correct order
+    for (int i = 0; i < numSize; ++i) {
         std::cout << num[i];
     }
-    for (int i = 0; i < smallLSize; i++) {
-        std::cout << smallL[i];
+    for (int i = 0; i < smallLettersSize; ++i) {
+        std::cout << smallLetters[i];
     }
-    for (int i = 0; i < bigLSize; i++) {
-        std::cout << bigL[i];
+    for (int i = 0; i < bigLettersSize; ++i) {
+        std::cout << bigLetters[i];
     }
     delete[] num;
-    delete[] bigL;
-    delete[] smallL;
+    delete[] bigLetters;
+    delete[] smallLetters;
 }
 
