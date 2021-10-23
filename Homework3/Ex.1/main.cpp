da547026a85f90fbb57a02dda23fae3d3e17778d
OgnyanY <o.yonchev@abv.bg>
2021-10-23 20:22:21 +0300

    completed first task without comments

--- Homework3/Ex.1/main.cpp
+++ Homework3/Ex.1/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+long Cube (long num) {
+    return num * num * num;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+   long long points = 0;
+   std::cin >> points;
+
+   int presents = 0;
+   long num = 1;
+    while (points != 0){
+        if (Cube(num) < points) {
+            ++num;
+            continue;
+        }
+        else if (Cube(num) > points) {
+            points -= Cube(num - 1);
+            ++presents;
+            num = 1;
+        }
+        else {
+            points -= Cube(num);
+            ++presents;
+            num = 1;
+        }
+    }
+    std::cout << presents;
 }
