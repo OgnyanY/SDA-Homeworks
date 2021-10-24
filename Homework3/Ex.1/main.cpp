fbbf08a68f679c800618d0b63910906d8b029e18
OgnyanY <o.yonchev@abv.bg>
2021-10-24 16:22:27 +0300

    completed first task

--- Homework3/Ex.1/main.cpp
+++ Homework3/Ex.1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//count cube of number
 long Cube (long num) {
     return num * num * num;
 }
@@ -11,15 +12,18 @@ int main() {
    int presents = 0;
    long num = 1;
     while (points != 0){
+        //this is when the value is not enough
         if (Cube(num) < points) {
             ++num;
             continue;
         }
+        //when the value is bigger than points get the less value
         else if (Cube(num) > points) {
             points -= Cube(num - 1);
             ++presents;
             num = 1;
         }
+        //when the value is the same points get the less value
         else {
             points -= Cube(num);
             ++presents;
