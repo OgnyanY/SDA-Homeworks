402b657225a8a973762a2cfbc5cf4fb95542447c
OgnyanY <o.yonchev@abv.bg>
2021-11-07 21:42:40 +0200

    finished Task1

--- Homework5/Task1/main.cpp
+++ Homework5/Task1/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::string lectures;
+    std::cin >> lectures;
+    std::vector<std::pair<int, int>> unique;
+    //histogram for ascii symbols
+    std::vector<std::pair<int,int>> ascii(127);
+
+    int maxASCII = 0;// max ascii for lecture
+    int minASCII = 0;// min ascii for lecture
+    for (int i = 0; i < lectures.size(); ++i) {
+        int code = (int)(unsigned char )lectures[i];//take ascii code
+        if(code > maxASCII){
+            maxASCII = code;
+        }
+        if(code < minASCII){
+            minASCII = code;
+        }
+        ++ascii[code].first;
+        ascii[code].second = i;
+    }
+
+    for (int i = minASCII; i <= maxASCII; ++i) {
+        if(ascii[i].first == 1) {//find unique
+            unique.push_back(ascii[i]);
+        }
+    }
+    //sort indexes
+    std::sort(unique.begin(),unique.end(),
+              [](std::pair<int, int>& left, std::pair<int, int>& right){
+        return left.second < right.second;
+    });
+
+    for (auto & i : unique) {
+        std::cout << i.second << " ";
+    }
 }
