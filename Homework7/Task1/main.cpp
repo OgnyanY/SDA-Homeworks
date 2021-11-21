d3ac1386dc358b879bb1ef25e5e5f8e3dc538c03
OgnyanY <o.yonchev@abv.bg>
2021-11-22 00:44:20 +0200

    completed Task1

--- Homework7/Task1/main.cpp
+++ Homework7/Task1/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <set>
+
+void find(std::set<int> &allSubMasks, int mask, int subMask) {
+    if (subMask < 0) {//if there is no more sub masks
+        return;
+    }
+    //find next possible sub mask
+    int nextSubMask = subMask & mask;
+    allSubMasks.insert(nextSubMask);
+    //continue until there is no more sub masks
+    find(allSubMasks, mask, nextSubMask - 1);
+}
+
+void findSubMasks(std::set<int> &allSubMasks, int mask) {
+    allSubMasks.insert(mask);//insert the mask as sub mask
+    int nextSubMask = mask - 1;//next eventually sub mask
+    find(allSubMasks, mask, nextSubMask);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int masksSize = 0;
+    std::cin >> masksSize;
+
+    std::set<int> allSubMasks;
+    //get masks
+    for (int i = 0; i < masksSize; ++i) {
+        int mask = 0;
+        std::cin >> mask;
+        findSubMasks(allSubMasks, mask);
+    }
+    //print the size of the sub masks
+    std::cout << allSubMasks.size();
     return 0;
 }
