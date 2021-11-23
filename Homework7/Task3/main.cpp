79b3f6c58444a602c6cec55a98b2f6d613c720c8
OgnyanY <o.yonchev@abv.bg>
2021-11-23 15:55:37 +0200

    slow solution for Task3

--- Homework7/Task3/main.cpp
+++ Homework7/Task3/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int numbersSize = 0, difference = 0;
+    std::cin >> numbersSize >> difference;
+
+    std::vector<int> numbers;
+    for (int i = 0; i < numbersSize; ++i) {
+        int num = 0;
+        std::cin >> num;
+        numbers.push_back(num);
+    }
+    bool find = false;
+    for (int i = 0; i < numbersSize; ++i) {
+        for (int j = i - 1; j >= 0; --j) {
+            if(numbers[i] > numbers[j] && numbers[i] - numbers[j] <= difference){
+                std::cout << j << ' ';
+                find = true;
+                break;
+            }
+        }
+        if(!find){
+            std::cout << -1 << ' ';
+        }
+        find = false;
+    }
+
     return 0;
 }
