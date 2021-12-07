799999a5f4e8d5b74f843e4f305aa41f8c0bd926
OgnyanY <o.yonchev@abv.bg>
2021-12-07 15:59:43 +0200

    unfinished Task2

--- Homework9/Task2/main.cpp
+++ Homework9/Task2/main.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
+#include <unordered_map>
+#include <set>
+
+struct Kilogram {
+    int value = 0;
+    int number = 0;
+    //int index = 0;
+
+    Kilogram(int value = 0, int number = 0) {
+        this->value = value;
+        this->number = number;
+    }
+
+    friend bool operator<(Kilogram const &lhs, Kilogram const &rhs) {
+        return lhs.value > rhs.value;
+    }
+
+    friend bool operator==(Kilogram const &lhs, Kilogram const &rhs) {
+        return lhs.value == rhs.value;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int animalsSize = 0, criterion = 0;
+    std::cin >> animalsSize >> criterion;
+    int result = 0;
+
+    std::unordered_map<int, int> animals;
+    std::set<int> sorted;
+    for (int i = 0; i < animalsSize; ++i) {
+        int animal;
+        std::cin >> animal;
+        sorted.insert(animal);
+        animals[animal] += 1;
+    }
+    std::set<int>::iterator();
+    for (auto first = sorted.begin(); first != --(--sorted.end()); first++) {
+
+        for (auto second = first; second != --sorted.end();) {
+            ++second;
+            if (*second == *first * criterion) {
+
+                for (auto third = second; third != sorted.end();) {
+                    ++third;
+                    if (*third == *second * criterion) {
+                        result += animals[*first] * animals[*second] * animals[*third];
+                        break;
+                    } else if (*third < *second * criterion) {
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+            } else if (*second < *first * criterion) {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+    }
+    std::cout << result;
+
     return 0;
 }
