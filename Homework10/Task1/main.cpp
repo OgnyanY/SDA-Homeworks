8651022188b04935934fdce86b7853da039ba857
OgnyanY <o.yonchev@abv.bg>
2021-12-14 15:53:51 +0200

    uncompleted Task1

--- Homework10/Task1/main.cpp
+++ Homework10/Task1/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <bits/stdc++.h>
+//#include <vector>
+#include <set>
+#include <unordered_map>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int applicationSize = 0;
+    std::cin >> applicationSize;
+
+    std::unordered_map<int, std::set<int>> applications;
+    bool answer = false;
+
+    for (int i = 0; i < applicationSize; ++i) {
+        int edges = 0;
+        std::cin >> edges;
+        for (int j = 0; j < edges; ++j) {
+            int start = 0, end = 0;
+            std::cin >> start >> end;
+            applications[end].insert(start);
+            for (auto it = applications[start].begin(); it != applications[start].end(); ++it) {
+                applications[end].insert(*it);
+            }
+
+            if (applications[end].count(end) > 0) {
+                answer = true;
+                std::cout << true << ' ';
+                break;
+            }
+        }
+        if (!answer) {
+            std::cout << false << ' ';
+        }
+        applications.clear();
+    }
 }
+
